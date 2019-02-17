d5eac3a8f1a285b8e9813470fb0a6c34c28d779f
Tuan Vu <tuanvu216@yahoo.com>
2019-02-16 16:46:23 -0800

    Update example_variables.py

--- examples/intro-example/dags/example_variables.py
+++ examples/intro-example/dags/example_variables.py
@@ -24,7 +24,7 @@ dag = DAG('example_variables',
 # var2 = [1, 2, 3]
 # var3 = {'k': 'value3'}
 
-## Better
+## 3 DB connections called
 # var1 = Variable.get("var1")
 # var2 = Variable.get("var2")
 # var3 = Variable.get("var3")
@@ -34,10 +34,6 @@ dag_config = Variable.get("example_variables_config", deserialize_json=True)
 var1 = dag_config["var1"]
 var2 = dag_config["var2"]
 var3 = dag_config["var3"]
-
-# print (type(var1))
-# print (type(var2))
-# print (type(var3))
 
 start = DummyOperator(
     task_id="start",
