8a42eadafbfe56d713b7143800d0329d2be94b91
tuanavu <tuanvu216@yahoo.com>
2021-08-16 13:47:51 -0700

    Comment out example_variables.py to avoid missing variable error
    Comment out example_variables.py to avoid missing variable error

--- examples/intro-example/dags/example_variables.py
+++ examples/intro-example/dags/example_variables.py
@@ -18,50 +18,50 @@ dag = DAG('example_variables',
     default_args=default_args)
 
 
-# Config variables
-## Common
+# # Config variables
+# # Common
 # var1 = "value1"
 # var2 = [1, 2, 3]
 # var3 = {'k': 'value3'}
 
-## 3 DB connections called
+# # 3 DB connections called
 # var1 = Variable.get("var1")
 # var2 = Variable.get("var2")
 # var3 = Variable.get("var3")
 
-## Recommended way
-dag_config = Variable.get("example_variables_config", deserialize_json=True)
-var1 = dag_config["var1"]
-var2 = dag_config["var2"]
-var3 = dag_config["var3"]
+# ## Recommended way
+# dag_config = Variable.get("example_variables_config", deserialize_json=True)
+# var1 = dag_config["var1"]
+# var2 = dag_config["var2"]
+# var3 = dag_config["var3"]
 
-start = DummyOperator(
-    task_id="start",
-    dag=dag
-)
+# start = DummyOperator(
+#     task_id="start",
+#     dag=dag
+# )
 
-# To test this task, run this command:
-# docker-compose run --rm webserver airflow test example_variables get_dag_config 2019-02-15
-t1 = BashOperator(
-    task_id="get_dag_config",
-    bash_command='echo "{0}"'.format(dag_config),
-    dag=dag,
-)
+# # To test this task, run this command:
+# # docker-compose run --rm webserver airflow test example_variables get_dag_config 2019-02-15
+# t1 = BashOperator(
+#     task_id="get_dag_config",
+#     bash_command='echo "{0}"'.format(dag_config),
+#     dag=dag,
+# )
 
-# You can directly use a variable from a jinja template
-## {{ var.value.<variable_name> }}
+# # You can directly use a variable from a jinja template
+# ## {{ var.value.<variable_name> }}
 
-t2 = BashOperator(
-    task_id="get_variable_value",
-    bash_command='echo {{ var.value.var3 }} ',
-    dag=dag,
-)
+# t2 = BashOperator(
+#     task_id="get_variable_value",
+#     bash_command='echo {{ var.value.var3 }} ',
+#     dag=dag,
+# )
 
-## {{ var.json.<variable_name> }}
-t3 = BashOperator(
-    task_id="get_variable_json",
-    bash_command='echo {{ var.json.example_variables_config.var3 }} ',
-    dag=dag,
-)
+# ## {{ var.json.<variable_name> }}
+# t3 = BashOperator(
+#     task_id="get_variable_json",
+#     bash_command='echo {{ var.json.example_variables_config.var3 }} ',
+#     dag=dag,
+# )
 
-start >> [t1, t2, t3]
+# start >> [t1, t2, t3]
